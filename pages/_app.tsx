7cb8ddba1e91b35a95d1fb96bc96274f5873a5fe
Huy-Thai <huythai.dev@gmail.com>
2022-12-12 19:59:24 +0700

    add new font and add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,32 @@ import '../styles/css/base.css';
 import '../styles/css/plugins.css';
 import '../styles/css/style.css';
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
+
+const env = process.env.NODE_ENV || 'test';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {env === 'production' ?? (
+        <>
+          {/* Global site tag (gtag.js) - Google Analytics */}
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-TNLY5QDP8G"
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-TNLY5QDP8G');
+        `}
+          </Script>
+        </>
+      )}
+      <Component {...pageProps} />
+    </>
+  );
 }
