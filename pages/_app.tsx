748b7f2bdcef0ca61002ca77ec37b94d8dabd3ef
Huy-Thai <huythai.dev@gmail.com>
2022-12-13 08:59:39 +0700

    update config google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,13 +8,13 @@ import '../styles/css/style.css';
 import type { AppProps } from 'next/app';
 import Script from 'next/script';
 
-const env = process.env.NODE_ENV || 'test';
+// const env = process.env.NODE_ENV || 'test';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      {env === 'production' ?? (
-        <>
+      {/* {env === 'production' ?? (
+        <> */}
           {/* Global site tag (gtag.js) - Google Analytics */}
           <Script
             src="https://www.googletagmanager.com/gtag/js?id=G-TNLY5QDP8G"
@@ -29,8 +29,8 @@ export default function App({ Component, pageProps }: AppProps) {
           gtag('config', 'G-TNLY5QDP8G');
         `}
           </Script>
-        </>
-      )}
+        {/* </>
+      )} */}
       <Component {...pageProps} />
     </>
   );
